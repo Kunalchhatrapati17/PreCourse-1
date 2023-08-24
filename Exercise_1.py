b27b25c3af2c18fa740cfe34324f612253b63fbb
Kunalchhatrapati17 <54681555+Kunalchhatrapati17@users.noreply.github.com>
2023-08-24 11:45:16 -0700

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,33 @@
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
+  #Time and Space complexity of each file
+  #Time Complexity:- O(1) for every method accept show method as we have to iterate through n elements 
+  # Space Complexity:- O(n) as we have to store n no. of elements in the list
      def __init__(self):
+       self.elements=[]
          
      def isEmpty(self):
+       return if(len.elements)==0
          
      def push(self, item):
-         
+         self.elements.append(item)
+       
      def pop(self):
-        
+         if not self.isEmpty():
+            return self.elements.pop()
+         else:
+             return "Empty Stack"
         
      def peek(self):
-        
+        if not self.isEmpty():
+          return self.elements.pop[-1]
+        else:
+          return "Stack empty"
+          
      def size(self):
+       return len(self.elements)
          
      def show(self):
-         
+       return self.elements  
 
 s = myStack()
 s.push('1')
